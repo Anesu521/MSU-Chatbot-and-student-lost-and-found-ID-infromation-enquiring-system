d09fbe8f059ad166228c4bbf6969624ce67fbab5
AnesuPhiri521 <sehphirry@gmail.com>
2019-12-18 09:38:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "change_password";
 $dbname = "msu";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
